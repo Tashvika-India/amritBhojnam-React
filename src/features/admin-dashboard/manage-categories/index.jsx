5b094a2919b5b116b384450809ded0e1eb11149b
Ankit-fictivebox <ankit@fictivebox.com>
2024-09-20 17:33:44 +0530

    created category modal

--- src/features/admin-dashboard/manage-categories/index.jsx
+++ src/features/admin-dashboard/manage-categories/index.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "@/components/ui/Heading";
 import YellowButton from "@/components/buttons/YellowButton";
 import CategoriesTable from "./components/CategoriesTable";
+import AddCategoryModal from "./components/AddCategoryModal";
 
 function ManageCategories() {
+  
+  const [visible, setVisible] = useState(false);
+  
+
   return (
     <>
       <div className="mt-3 mb-5 row">
@@ -11,17 +16,18 @@ function ManageCategories() {
           <Heading value={"Categories"} />
         </div>
         <div className="col-md-6 text-end">
-          <YellowButton lable={"+ Add New Category"} />
+          <YellowButton handleClick={() => setVisible(true)} lable={"+ Add New Category"} />
         </div>
       </div>
 
       <div className="">
-         <div className="card">
+        <div className="card">
           <div className="card-body">
-             <CategoriesTable />
+            <CategoriesTable />
           </div>
-         </div>
+        </div>
       </div>
+      <AddCategoryModal visible={visible} setVisible={setVisible} />
     </>
   );
 }
