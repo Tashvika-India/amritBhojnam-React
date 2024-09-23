cdd1a4bedb766ad1eb1765b2cbe6e30725d968ff
Ankit-fictivebox <ankit@fictivebox.com>
2024-09-23 13:35:04 +0530

    header

--- src/layout/web-layout/Header.jsx
+++ src/layout/web-layout/Header.jsx
@@ -6,6 +6,7 @@ import { IoSearchOutline } from 'react-icons/io5';
 import logo from '../../assets/images/web/logo.svg'
 import { FaRegHeart, FaRegUser } from 'react-icons/fa';
 import { CgShoppingBag } from 'react-icons/cg';
+import { IoMdMenu } from 'react-icons/io';
 
 const Header = () => {
   const [selectedCity, setSelectedCity] = useState(null);
@@ -22,7 +23,7 @@ const Header = () => {
       <header className='fb-bottom-shadow'>
         <div className="header-top bg-yellow py-2">
           <div className="container fb-container d-flex flex-wrap gap-2 justify-content-center justify-content-lg-between align-items-center">
-            <p className='text-white fw-500 fb-fs-14'>Free delivery & 40% discount for next 3 orders! Place your 1st order now.</p>
+            <p className='text-white fw-500 fb-fs-14 d-none d-lg-block'>Free delivery & 40% discount for next 3 orders! Place your 1st order now.</p>
             <p className='text-white fw-500 fb-fs-14'>Need Help? Call Us: <a className='text-white text-decoration-none' href="tel:+1800 900 5600">+1800 900 5600</a></p>
           </div>
         </div>
@@ -31,56 +32,87 @@ const Header = () => {
             <div className="logo">
               <img src={logo} alt="logo" className='img-fluid' />
             </div>
-            <div className="header-delivery-search mx-auto d-inline-flex">
-              <div className="header-delivery d-inline-flex gap-3 align-items-center">
-                <div className="header-delivery-icon">
-                  <CiLocationOn size={25} />
+            <div className="header-delivery-search mx-auto  d-none d-xl-block">
+              <div className="d-inline-flex gap-4 w-100">
+                <div className="header-delivery d-inline-flex gap-3 align-items-center">
+                  <div className="header-delivery-icon">
+                    <CiLocationOn size={25} />
+                  </div>
+                  <div className="header-delivery-text">
+                    <small>Deliver to</small>
+                    <p className='fw-600 text-truncate'>Akshya Nagar 1st Block 1...</p>
+                  </div>
                 </div>
-                <div className="header-delivery-text">
-                  <small>Deliver to</small>
-                  <p className='fw-600'>Akshya Nagar 1st Block 1...</p>
-                </div>
-              </div>
-              <div className="header-search d-inline-flex border p-2">
-                <div className="all-category">
-                  <Dropdown value={selectedCity} onChange={(e) => setSelectedCity(e.value)} options={cities} optionLabel="name"
-                    placeholder="All Categories" className="w-full border-0" />
-                </div>
-                <div className="search-input position-relative z-1" >
-                  <InputText keyfilter="int" placeholder="Search for products" className='border-0' />
-                  <span className='search-icon z-2 position-absolute end-0 top-50 translate-middle mb-3'><IoSearchOutline /></span>
+                <div className="header-search d-inline-flex w-100 align-self-center">
+                  <div className="all-category">
+                    <Dropdown value={selectedCity} onChange={(e) => setSelectedCity(e.value)} options={cities} optionLabel="name"
+                      placeholder="All Categories" className="w-full border-0" />
+                  </div>
+                  <div className="search-input position-relative z-1 w-100" >
+                    <InputText keyfilter="int" placeholder="Search for products" className='border-0 ps-3' />
+                    <span className='search-icon z-2 position-absolute end-0 top-50 translate-middle mb-3'><IoSearchOutline color="#918e92" size={"1.25rem"} /></span>
+                  </div>
                 </div>
               </div>
             </div>
             <div className="header-actions">
-              <ul className='list-unstyled d-flex align-items-center justify-content-between gap-3'>
+              <ul className='list-unstyled align-items-center justify-content-between gap-4 web-header-actions d-none d-xl-flex'>
                 <li>
                   <a href="#" className='d-inline-flex flex-column justify-content-center align-items-center'>
-                    <FaRegUser />
-                    <span className='d-inline-block'>Account</span>
+                    <FaRegUser size={"1.625rem"} />
+                    <span className='d-inline-block fb-fs-14 fw-600'>Account</span>
                   </a>
                 </li>
                 <li>
                   <a href="#" className='d-inline-flex flex-column justify-content-center align-items-center'>
-                    <FaRegHeart />
-                    <span className='d-inline-block'>Wishlist</span>
+                    <FaRegHeart size={"1.625rem"} />
+                    <span className='d-inline-block fb-fs-14 fw-600'>Wishlist</span>
                   </a>
                 </li>
                 <li>
                   <a href="#" className='d-inline-flex flex-column justify-content-center align-items-center'>
-                    <CgShoppingBag />
-                    <span className='d-inline-block'>My Cart</span>
+                    <div className="position-relative">
+                      <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-yellow">
+                        1
+                        <span class="visually-hidden">unread messages</span>
+                      </span>
+                      <CgShoppingBag size={"1.625rem"} />
+                    </div>
+                    <span className='d-inline-block fb-fs-14 fw-600'>My Cart</span>
+                  </a>
+                </li>
+              </ul>
+              <ul className='list-unstyled d-flex d-xl-none align-items-center justify-content-between gap-4 mobile-header-actions'>
+                <li>
+                  <a href="#" className='d-inline-flex flex-column justify-content-center align-items-center'>
+                    <IoSearchOutline size={"1.625rem"} /> 
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className='d-inline-flex flex-column justify-content-center align-items-center'>
+                    <div className="position-relative">
+                      <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-yellow">
+                        1
+                        <span class="visually-hidden">unread messages</span>
+                      </span>
+                      <CgShoppingBag size={"1.625rem"} />
+                    </div> 
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className='d-inline-flex flex-column justify-content-center align-items-center'>
+                    <IoMdMenu  size={"1.625rem"} /> 
                   </a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
-        <div className="header-bottom py-4 border-top">
+        <div className="header-bottom py-4 border-top d-none d-xl-block">
           <div className="container fb-container">
             <div className="header-divider d-flex justify-content-between ">
               <div className="header-link-list">
-                <ul className='d-flex gap-4'>
+                <ul className='d-flex gap-5'>
                   <li><a href="#">Home</a></li>
                   <li><a href="#">Shop</a></li>
                   <li><a href="#">Best Deals</a></li>
@@ -90,15 +122,15 @@ const Header = () => {
                 </ul>
               </div>
               <div className="header-divider-action">
-                <ul className='d-flex gap-4'>
+                <ul className='d-flex gap-5'>
                   <li><a href="#">Track Your Order</a></li>
-                  <li><a href="#" className='text-orange'>Almost Finished <span className='text-white d-inline-block lh-normal fb-fs-14 bg-orange rounded px-2 text-uppercase ms-2'>SALE</span></a></li>
+                  <li><a href="#" className='text-orange'>Almost Finished <span className='ms-2 text-white text-uppercase badge bg-orange fb-fs-14 fw-500'>SALE</span></a></li>
                 </ul>
               </div>
             </div>
           </div>
         </div>
-      </header>
+      </header >
     </>
   )
 }
