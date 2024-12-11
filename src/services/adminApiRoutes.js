36e288078c13d4023b78618d8d55524ead234f5b
Ankit-fictivebox <ankit@fictivebox.com>
2024-12-11 19:18:56 +0530

    update api added

--- src/services/adminApiRoutes.js
+++ src/services/adminApiRoutes.js
@@ -166,9 +166,6 @@ export const putProfileApi = (id,payload) => API.put(`/profiles/${id}/`, payload
 
 // pay now 
 export const postPayNowApi = (payload) => API.post(`/accounts/payu/initiate/`, payload); 
-export const postPayuCallbackApi = (url,payload) => API.post(url, payload);
- 
-export const postPayuCallbackApi = (payload) => API.post(`/accounts/payu/callback/`, payload);
-
+export const postPayuCallbackApi = (url,payload) => API.post(url, payload); 
 //order history
 export const getOrderApi = () => API.get(`/orders/`); 
