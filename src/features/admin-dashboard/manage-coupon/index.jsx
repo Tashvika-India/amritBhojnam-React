95ce3a48b597eee7bd9e40cd26d4e16cdb072741
Ankit-fictivebox <ankit@fictivebox.com>
2024-12-15 16:24:30 +0530

    updated created coupon

--- src/features/admin-dashboard/manage-coupon/index.jsx
+++ src/features/admin-dashboard/manage-coupon/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import Heading from "@/components/ui/Heading";
+import YellowButton from '../../../components/buttons/YellowButton'; 
+import CouponTable from './components/CouponTable';
 
-const index = () => {
+const AdminCoupon = () => {
     return (
-        <div>  
-            
-        </div>
+        <>
+            <div className="mt-3 mb-5 row">
+                <div className="col-md-6">
+                    <Heading value={"Coupons"} />
+                </div>
+                <div className="col-md-6 text-end">
+                    <YellowButton lable={"+ Add New Coupons"} />
+                </div>
+            </div>
+
+            <div className="">
+                <div className="card">
+                    <div className="card-body">
+                        <CouponTable />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
-export default index
+export default AdminCoupon;
