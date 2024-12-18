5effb92caedd2f8eb022c25a67caa2b6f926ae8f
shivani chauhan <shivani.chauhan@fictivebox.com>
2024-12-18 19:00:22 +0530

    category tabs

--- src/features/admin-dashboard/manage-categories/index.jsx
+++ src/features/admin-dashboard/manage-categories/index.jsx
@@ -5,12 +5,17 @@ import CategoriesTable from "./components/CategoriesTable";
 import AddCategoryModal from "./components/AddCategoryModal";
 import { getCategoriesApi, patchCategoriesApi } from "../../../services/adminApiRoutes";
 import Loading from "../../../components/ui/Loading";
+import TabsButtons from "../../../components/ui/TabsButton";
+import { InputText } from "primereact/inputtext";
+import useURLFilters from "../../../custom-compoents/useURLFilters";
 
 function ManageCategories() {
   const [visible, setVisible] = useState(false);
   const [categories, setCategories] = useState([]);
   const [editData, setEditData] = useState(null);
+  const [filter, setFilter] = useURLFilters([]);
   const [loading, setLoading] = useState(false);
+  const [activeTab, setActiveTab] = useState("Active Orders");
 
   async function getCategories() {
     setLoading(true);
@@ -77,6 +82,36 @@ function ManageCategories() {
       <div className="">
         <div className="card">
           <div className="card-body">
+          <div className="row mb-3">
+              <div className="col-md-4">
+                <div>
+                  <div className="mb-3">
+                    <TabsButtons
+                      activeTab={activeTab}
+                      setActiveTab={setActiveTab}
+                      labelOne={"Active Orders"}
+                      labelTwo={"New Orders"}
+                    />
+                  </div>
+                  {/* {activeTab === "Active Orders" && <ProductTable />}
+                  {activeTab === "New Orders" && <ProductTable />} */}
+                </div>
+              </div>
+              <div className="col-md-2">
+              </div>
+              <div className="col-md-2 text-end">
+              </div>
+              <div className="col-md-3 ms-auto text-end">
+                <InputText
+                className="w-100"
+                  value={filter.name}
+                  onChange={(e) =>
+                    setFilter({ ...filter, name: e.target.value })
+                  }
+                  placeholder="Search Product"
+                />
+              </div>
+            </div>
             {loading ? (
               <Loading />
             ) : (
