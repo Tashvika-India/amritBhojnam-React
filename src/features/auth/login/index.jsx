fa3b072c294482040276225e8851108214165c39
Ankit-fictivebox <ankit@fictivebox.com>
2024-12-22 00:00:23 +0530

    updated responsive for live changes

--- src/features/auth/login/index.jsx
+++ src/features/auth/login/index.jsx
@@ -6,29 +6,42 @@ import LoginForm from "./LoginForm";
 function Login() {
   return (
     <>
-      <div className="login-wrapper">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-6">
+      {/* <div className="login-wrapper">
+        <div className="">
+          <div className="d-flex">
+            <div className="col">
               <div className="left-section">
-                <div className="mt-xxl-5 mt-3">
-                  <img src={mainLogo} alt="Main Logo" className="img-fluid" />
-                  <div className="mb-xxl-5 mb-3">
-                    <h2 className="mt-5">
-                      Welcome to <span className="text-orange fw-600">Amrit Bhojanam Management System</span>
-                    </h2>
-                    <p className="mt-4">Enter your email and password for login</p>
-                  </div>
-                  <LoginForm />
+                
                 </div>
               </div>
             </div>
-            <div className="col-md-6">
+            <div className="col">
               <img src={loginPageImage} alt="Login Page Image" className="img-fluid admin-login-image" />
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
+      <section className="container-fluid login-wrapper-conatiner">
+        <div className="row align-items-center">
+          <div className="col-lg-4 col-11 mx-auto">
+            <div className="left-section">
+              <img src={mainLogo} alt="Main Logo" className="img-fluid login-main-logo" />
+              <div className="mb-xxl-5 mb-3">
+                <h2 className="mt-5">
+                  Welcome to <span className="text-orange fw-600">Amrit Bhojanam Management System</span>
+                </h2>
+                <p className="mt-4">Enter your email and password for login</p>
+              </div>
+              <LoginForm />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="mt-2">
+              <img src={loginPageImage} alt="Login Page Image" className="img-fluid admin-login-image p-4" />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
