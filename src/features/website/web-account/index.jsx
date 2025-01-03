1383143d5ff2ea8a6fad1eecb8a42e790b6855a4
Ankit-fictivebox <ankit@fictivebox.com>
2025-01-03 17:42:57 +0530

    updated Order review and product detail review with add images and comment with the star rating

--- src/features/website/web-account/index.jsx
+++ src/features/website/web-account/index.jsx
@@ -39,6 +39,7 @@ import { Dialog } from "primereact/dialog";
 import { Rating } from "primereact/rating";
 import ReviewModal from "../../../components/ui/ReviewModal";
 import { notifySuccess } from "../../../components/ui/Notification";
+import Loading from "../../../components/ui/Loading";
 
 const UserProfile = () => {
   const [loading, setLoading] = useState(false);
@@ -103,7 +104,7 @@ const UserProfile = () => {
       }
       setLoading(false);
       setOpen(false);
-      notifySuccess("Address added Successfully"); 
+      notifySuccess("Address added Successfully");
       scrollTo(0, 0);
       formik.resetForm();
     } catch (error) {
@@ -125,7 +126,7 @@ const UserProfile = () => {
       setLoading(false);
       scrollTo(0, 0);
       setOpen(false);
-      notifySuccess("Address updated Successfully"); 
+      notifySuccess("Address updated Successfully");
       formik.resetForm();
     } catch (error) {
       console.error("Error submitting form:", error);
@@ -231,9 +232,9 @@ const UserProfile = () => {
     }
   };
 
-  const handleReviewClick = (id) => {
+  const handleReviewClick = (id,name,image) => {
     setVisible(true);
-    setProductId(id);
+    setProductId({id,name,image});
   };
 
   const handleImageChange = (event) => {
@@ -341,7 +342,7 @@ const UserProfile = () => {
                   label="1"
                 >
 
-                  <span className="me-1">
+                  <span className="me-2">
                     <i className="pi pi-user"></i>
                   </span>
                   My Account
@@ -357,7 +358,7 @@ const UserProfile = () => {
                   label="2"
                 >
 
-                  <span className="me-1">
+                  <span className="me-2">
                     <i className="pi pi-box"></i>
                   </span>
                   Order History
@@ -373,7 +374,7 @@ const UserProfile = () => {
                   label="3"
                 >
 
-                  <span className="me-1">
+                  <span className="me-2">
                     <i className="pi pi-map-marker"></i>
                   </span>
                   Address Book
@@ -382,8 +383,7 @@ const UserProfile = () => {
               <TabView
                 className="custom-tabview"
                 activeIndex={activeIndex}
-                onTabChange={(e) => setActiveIndex(e.index)}
-              >
+                onTabChange={(e) => setActiveIndex(e.index)}>
                 <TabPanel header="My Account">
                   <div className="account-section mb-md-4">
                     <div className="d-flex justify-content-between">
@@ -457,13 +457,13 @@ const UserProfile = () => {
                             <TextField
                               fullWidth
                               className="rounded-20 me-5"
-                              id="pp" 
+                              id="pp"
                               name="pp"
                               variant="outlined"
                               disabled={!profileEdit}
-                              type="file" 
-                              onChange={handleImageChange} 
-                            /> 
+                              type="file"
+                              onChange={handleImageChange}
+                            />
                           </div>
                           <div className="col-md-4 mb-4">
                             <FormControl fullWidth>
@@ -537,125 +537,124 @@ const UserProfile = () => {
                 <TabPanel header="Order History">
                   <div className="order-section">
                     <p className="fb-fs-26 fw-bold my-4">Order History</p>
-                    {order.map((item, index) => (
-                      <div className="summary-card rounded-20 mb-4" key={index}>
-                        <div className="container">
-                          <div className="row border-bottom px-2 px-md-3 py-3">
-                            <div className="col-md-3">
-                              <p>
-                                Order ID:
-                                <span className="fw-600" title={item?.id}>
-                                  &nbsp;&nbsp;
-                                  {item?.id?.slice(0, 12)}...
-                                </span>
-                              </p>
-                            </div>
-                            <div className="col-md-3">
-                              <p>
-                                Order Placed:
-                                <span className="fw-600">
-                                  &nbsp;&nbsp;
-                                  {new Date(
-                                    item?.created_at
-                                  ).toLocaleDateString("en-GB")}
-                                </span>
-                              </p>
-                            </div>
-                            <div className="col-6 col-md-3">
-                              <p>
-                                Total Amount:
-                                <span className="fw-600">
-                                  &nbsp;&nbsp; ₹ {item?.amount_to_pay}
-                                </span>
-                              </p>
-                            </div>
-                            <div className="col-6 col-md-3 text-end">
-                              <a href="profile?tab=orders/" download>
-
-                                <p className="text-orange fw-500">
-                                  Download Invoice
+                    {loading ?
+                      <Loading />
+                      : order.map((item, index) => (
+                        <div className="summary-card rounded-20 mb-4" key={index}>
+                          <div className="container">
+                            <div className="row border-bottom px-2 px-md-3 py-3">
+                              <div className="col-md-3">
+                                <p>
+                                  Order ID:
+                                  <span className="fw-600" title={item?.id}>
+                                    &nbsp;&nbsp;
+                                    {item?.id?.slice(0, 12)}...
+                                  </span>
                                 </p>
-                              </a>
+                              </div>
+                              <div className="col-md-3">
+                                <p>
+                                  Order Placed:
+                                  <span className="fw-600">
+                                    &nbsp;&nbsp;
+                                    {new Date(
+                                      item?.created_at
+                                    ).toLocaleDateString("en-GB")}
+                                  </span>
+                                </p>
+                              </div>
+                              <div className="col-6 col-md-3">
+                                <p>
+                                  Total Amount:
+                                  <span className="fw-600">
+                                    &nbsp;&nbsp; ₹ {item?.amount_to_pay}
+                                  </span>
+                                </p>
+                              </div>
+                              <div className="col-6 col-md-3 text-end align-self-end">
+                                <a href="profile?tab=orders/" download>
+                                  <p className="text-orange fw-500">
+                                    Download Invoice
+                                  </p>
+                                </a>
+                              </div>
                             </div>
-                          </div>
-                          {item?.product_details.map((data) => (
-                            <div className="border-bottom">
-                              <div className="row px-2 px-md-3 pt-3 py-md-4">
-                                <div className="col-md-8">
-                                  <div className="prod-detail d-flex align-items-center">
-                                    <img
-                                      className="img-fluid me-4 rounded-4"
-                                      style={{ height: "6rem", width: "6rem" }}
-                                      src={data?.product?.images[0]?.image}
-                                      alt="pencil"
-                                    />
-                                    <div>
-                                      <p className="fb-fs-18 fw-600 text-dark-grey">
-                                        {data?.product?.name}
-                                      </p>
-                                      <p className="mt-2">
-                                        Qty:
-                                        <span className="fw-600">
-
-                                          {data?.item_quantity}
-                                        </span>
-                                      </p>
-                                      <p className="mt-2">
-                                        Size:
-                                        <span className="fw-600">
-
-                                          {`${data?.product?.quantity}${data?.product?.quantity_unit}`}
-                                        </span>
+                            {item?.product_details.map((data) => (
+                              <div className="border-bottom">
+                                <div className="row px-2 px-md-3 pt-3 py-md-4">
+                                  <div className="col-md-8">
+                                    <div className="prod-detail d-flex align-items-center">
+                                      <img
+                                        className="img-fluid me-4 rounded-4"
+                                        style={{ height: "6rem", width: "6rem" }}
+                                        src={data?.product?.images[0]?.image}
+                                        alt="pencil"
+                                      />
+                                      <div>
+                                        <p className="fb-fs-18 fw-600 text-dark-grey">
+                                          {data?.product?.name}
+                                        </p>
+                                        <p className="mt-2">
+                                          Qty:
+                                          <span className="fw-600">
+                                            {data?.item_quantity}
+                                          </span>
+                                        </p>
+                                        <p className="mt-2">
+                                          Size:
+                                          <span className="fw-600">
+                                            {`${data?.product?.quantity}${data?.product?.quantity_unit}`}
+                                          </span>
+                                        </p>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div className="col-md-4">
+                                    <div className="price-sec text-end text-dark-grey">
+                                      <p className="fb-fs-24 fw-bold">
+                                        ₹{data?.price}
                                       </p>
                                     </div>
                                   </div>
                                 </div>
-                                <div className="col-md-4">
-                                  <div className="price-sec text-end text-dark-grey">
-                                    <p className="fb-fs-24 fw-bold">
-                                      ₹{data?.price}
-                                    </p>
+                                <div className="row m-md-3">
+                                  <div className="col-md-6">
+                                    <div className="d-flex mb-2 mb-md-0">
+                                      <img
+                                        className="img-fluid me-2"
+                                        style={{
+                                          height: "1.3rem",
+                                          width: "1.3rem",
+                                          aspectRatio: "1/1",
+                                        }}
+                                        src={tickImg}
+                                        alt="pencil"
+                                      />
+                                      <p className="text-dark-grey">
+                                        Delivered within 5-7 days
+                                        {/* {new Date(item?.delivered_on).toLocaleDateString("en-US", {year: "numeric",month: "long",day: "numeric",})} */}
+                                      </p>
+                                    </div>
                                   </div>
-                                </div>
-                              </div>
-                              <div className="row m-md-3">
-                                <div className="col-md-6">
-                                  <div className="d-flex mb-2 mb-md-0">
-                                    <img
-                                      className="img-fluid me-2"
-                                      style={{
-                                        height: "1.3rem",
-                                        width: "1.3rem",
-                                        aspectRatio: "1/1",
-                                      }}
-                                      src={tickImg}
-                                      alt="pencil"
-                                    />
-                                    <p className="text-dark-grey">
-                                      Delivered within 5-7 days
-                                      {/* {new Date(item?.delivered_on).toLocaleDateString("en-US", {year: "numeric",month: "long",day: "numeric",})} */}
-                                    </p>
-                                  </div>
-                                </div>
-                                <div className="col-md-6 text-md-end">
-                                  <div className="more-option d-flex mb-2 mb-lg-0 justify-content-md-end">
-                                    <button onClick={() => handleReviewClick(data?.product?.id)} className="fw-500 text-center border-0 text-dark-grey bg-transparent border-end  pe-md-4">
-                                      Add Review
-                                    </button>
-                                    <Link to={`/product-detail?product_id=${data?.product?.id}`} className="fw-500 text-center border-0 text-dark-grey bg-transparent border-end   px-md-4">
-                                      View Product
-                                    </Link>
-                                    <button className="fw-500 text-center border-0 text-orange bg-transparent ms-lg-3">
+                                  <div className="col-md-6 text-md-end">
+                                    <div className="more-option d-flex mb-2 mb-lg-0 justify-content-md-end">
+                                      <button onClick={() => handleReviewClick(data?.product?.id, data?.product?.name, data?.product?.images[0]?.image)} className="fw-500 text-center border-0 text-dark-grey bg-transparent border-end  pe-md-4">
+                                        Add Review
+                                      </button>
+                                      <Link to={`/product-detail?product_id=${data?.product?.id}`} className="fw-500 text-center border-0 text-dark-grey bg-transparent  ps-2 ps-md-4">
+                                        View Product
+                                      </Link>
+                                      {/* <button className="fw-500 text-center border-0 text-orange bg-transparent ms-lg-3">
                                       Buy Again
-                                    </button>
+                                    </button> */}
+                                    </div>
                                   </div>
                                 </div>
                               </div>
-                            </div>
-                          ))}
+                            ))}
+                          </div>
                         </div>
-                      </div>
-                    ))}
+                      ))}
                   </div>
                 </TabPanel>
                 <TabPanel header="Address Book">
@@ -777,8 +776,7 @@ const UserProfile = () => {
           </div>
         </div>
       </div>
-      <ReviewModal visible={visible} setVisible={setVisible} product_id={productId} />
-
+      <ReviewModal visible={visible} setVisible={setVisible} data={productId} />
       <Footer />
     </div>
   );
