a77a5ed83218466cd1d1f1b8684e1bb81eecf0df
Ankit-fictivebox <ankit@fictivebox.com>
2025-01-15 14:02:13 +0530

    updated banner skelton loading and wishlist loading

--- src/features/website/web-wishlist/index.jsx
+++ src/features/website/web-wishlist/index.jsx
@@ -8,11 +8,12 @@ import wishlistImage from "../../../assets/images/web/wishlist-empty.png";
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Loading from "../../../components/ui/Loading";
 
 const Wishlist = () => {
   const dispatch = useDispatch();
 
-  const { wishlist=[], loading } = useSelector((state) => state.wishlist);
+  const { wishlist = [], loading } = useSelector((state) => state.wishlist); 
 
   useEffect(() => {
     dispatch(fetchWishlist());
@@ -42,35 +43,35 @@ const Wishlist = () => {
           </div>
           <div></div>
           <div className="row">
-          {
-            wishlist?.length > 0 ?
-            <div className="col-12">
-              <WishList wishlist={wishlist} />
-            </div>
-            :
-            <div className="mx-auto col-md-8 mt-5 pt-5 pt-md-0">
-              <img
-                className="img-fluid mx-auto mb-5"
-                src={wishlistImage}
-                alt="empty-wishlist"
-              />
-              <p className="fb-fs-40 fw-600 text-center">
-                Your wishlist is empty
-              </p>
-              <p className="fb-fs-20 text-center text-mid-grey py-4">
-                Your wishlist is empty. Start adding your favourite products to
-                keep track <br></br> of what you love!
-              </p>
-              <div className="text-center">
-                <Link
-                  to="/products"
-                  className="button-primary  d-inline-block mt-2 px-5 text-center"
-                >
-                  Shop Now
-                </Link>
-              </div>
-            </div>
-          }
+            {
+              wishlist?.length > 0 ?
+                <div className="col-12">
+                  {loading ? <Loading/> : <WishList wishlist={wishlist} />}
+                </div>
+                :
+                <div className="mx-auto col-md-8 mt-5 pt-5 pt-md-0">
+                  <img
+                    className="img-fluid mx-auto mb-5"
+                    src={wishlistImage}
+                    alt="empty-wishlist"
+                  />
+                  <p className="fb-fs-40 fw-600 text-center">
+                    Your wishlist is empty
+                  </p>
+                  <p className="fb-fs-20 text-center text-mid-grey py-4">
+                    Your wishlist is empty. Start adding your favourite products to
+                    keep track <br></br> of what you love!
+                  </p>
+                  <div className="text-center">
+                    <Link
+                      to="/products"
+                      className="button-primary  d-inline-block mt-2 px-5 text-center"
+                    >
+                      Shop Now
+                    </Link>
+                  </div>
+                </div>
+            }
           </div>
         </div>
       </section>
