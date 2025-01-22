987317b7331c385428e94b8407e794b43b33878f
Ankit-fictivebox <ankit@fictivebox.com>
2025-01-22 19:11:41 +0530

    product thum slide and pincode check aviablity

--- src/utils/constant-variable.js
+++ src/utils/constant-variable.js
@@ -2,23 +2,38 @@ export const orange = "#F26722";
 export const baseURL = import.meta.env.VITE_BASE_API_URL;
 
 export const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+export const formatDateTime = (dateString) => {
+  const date = new Date(dateString);
+
+  const options = {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true, // Use 12-hour format
   };
-  
-export  const formatDateTime = (dateString) => {
-    const date = new Date(dateString);
-  
-    const options = {
-      day: "2-digit",
-      month: "short",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: true, // Use 12-hour format
-    };
-  
-    return date.toLocaleString("en-US", options).replace(",", "");
-  };
+
+  return date.toLocaleString("en-US", options).replace(",", "");
+};
+
+
+
+
+export const formatDeliveryDateCustom = (pinDate) => {
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+  const date = new Date(pinDate);
+  const dayName = days[date.getDay()];
+  const monthName = months[date.getMonth()];
+  const day = date.getDate();
+
+  return `${dayName}, ${monthName} ${day}`;
+};
