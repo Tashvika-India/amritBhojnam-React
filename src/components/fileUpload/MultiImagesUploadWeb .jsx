a9fa18ce32b3417cf3db7dc0897721042aeb51a1
Ankit-fictivebox <ankit@fictivebox.com>
2025-01-26 22:47:31 +0530

    removed uncesssary consoles

--- src/components/fileUpload/MultiImagesUploadWeb .jsx
+++ src/components/fileUpload/MultiImagesUploadWeb .jsx
@@ -17,8 +17,7 @@ export default React.memo(function MultiImagesUploadWeb ({ formik, name, baseURL
 
     try {
       setUploading(true);
-      const response = await multiImageUploadApi(formData);
-      console.log("Form" , response);
+      const response = await multiImageUploadApi(formData); 
       const uploadedFiles = response.data; // Assuming response contains the array of objects
       setFieldValue(name, [...(values[name] || []), ...uploadedFiles]);
     } catch (error) {
