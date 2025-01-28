f54a96249cc17227fd95fbcadb6116c196e8e117
shivani chauhan <shivani.chauhan@fictivebox.com>
2025-01-28 12:28:52 +0530

    product list price range slider ui

--- src/components/ui/ChildSlider.jsx
+++ src/components/ui/ChildSlider.jsx
@@ -12,6 +12,7 @@ export default function ChildSlider({ minPrice, maxPrice, onSliderChange }) {
     };
 
     return (
+        <div className="price-range-slider">
         <Box sx={{ width: "100%" }}>
             <Slider
                 getAriaLabel={() => "Price range"}
@@ -23,5 +24,6 @@ export default function ChildSlider({ minPrice, maxPrice, onSliderChange }) {
                 max={500}
             />
         </Box>
+        </div>
     );
 }
