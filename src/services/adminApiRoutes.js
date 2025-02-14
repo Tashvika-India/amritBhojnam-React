4f7ae65ce1660b792d737ff4a8bf2c7e0102dfe9
Ankit-fictivebox <ankit@fictivebox.com>
2025-02-14 18:28:58 +0530

    integrated the coin Managment with the post and get api

--- src/services/adminApiRoutes.js
+++ src/services/adminApiRoutes.js
@@ -248,3 +248,9 @@ export const getMealFoodSensitivityApi = () => API.get(`/meal_food_sensitivity/`
 export const postMealFoodSensitivity = (payload) => API.post(`/meal_food_sensitivity/`, payload);
 export const putMealFoodSensitivity = (id,payload) => API.put(`/meal_food_sensitivity/${id}/`, payload);
 export const deleteFoodSensitivity = (id) => API.delete(`/meal_food_sensitivity/${id}`);
+
+
+// amrit coin 
+
+export const getAmritCoinApi = () => API.get(`/coin_management/`);
+export const postAmritCoinApi = (payload) => API.post(`/coin_management/`, payload);
